d3f4c27538c36a9aa67178e095f9449ad7e85d70
orkrok <zeano1215@gmail.com>
2025-08-25 21:31:18 +0900

    first

---
+++ components/Logo.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <Link href="/" className="text-[28px] font-normal">
+      Jake<span className="text-accent text-4xl">.</span>
+    </Link>
+  );
+};
+
+export default Logo;
